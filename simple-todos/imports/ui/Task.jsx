2b30a3628a7fce03c40f8439ab971409a3967cb9
Megan Fanning <megantfanning@gmail.com>
2016-10-05 12:23:17 -0700

    accidentally deleted the deletion box- added it back in

--- simple-todos/imports/ui/Task.jsx
+++ simple-todos/imports/ui/Task.jsx
@@ -24,21 +24,32 @@ export default class Task extends Component {
       checked: this.props.task.checked,
       private: this.props.task.private,
     });
- 
+
     return (
-      <li className={taskClassName}> 
+      <li className={taskClassName}>
+        <button className="delete" onClick={this.deleteThisTask.bind(this)}>
+          &times;
+        </button>
+
+        <input
+          type="checkbox"
+          readOnly
+          checked={this.props.task.checked}
+          onClick={this.toggleChecked.bind(this)}
+        />
+
         { this.props.showPrivateButton ? (
           <button className="toggle-private" onClick={this.togglePrivate.bind(this)}>
             { this.props.task.private ? 'Private' : 'Public' }
           </button>
         ) : ''}
- 
+
         <span className="text">
           <strong>{this.props.task.username}</strong>: {this.props.task.text}
         </span>
- 
       </li>
     );
+
   }
 }
 
